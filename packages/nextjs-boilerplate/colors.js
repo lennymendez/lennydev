55309acd7c376d17e764ecf3e96d3ff80e04583d
Lenny Mendez <lennymendez@Lennys-MacBook-Pro.local>
2019-03-04 18:00:57 -0400

    create spacing and styles in lib

--- packages/nextjs-boilerplate/colors.js
+++
@@ -1,5 +0,0 @@
-export const PrototypeColors = {
-  background: "#cacaca",
-  foreground: "#eaeaea",
-  element: "#e4e4e4"
-};
---
+++ packages/nextjs-boilerplate/lib/spacing.js
@@ -0,0 +1,9 @@
+export const Spacing = {
+  xs: "4pt",
+  sm: "8pt",
+  md: "16pt",
+  lg: "32pt",
+  xl: "64pt",
+  xxl: "128pt",
+  xxxl: "256pt"
+};
